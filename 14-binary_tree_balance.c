1a13e7594d53af92c753f7c36cac94013b714a55
TobiLight <jgetitdonefast@gmail.com>
2023-08-30 17:57:56 +0100

    Measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,6 +5,31 @@
 
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height.
+ *
+ * Return: Height of tree otherwise 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0, height_right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		height_left = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		height_right = binary_tree_height(tree->right) + 1;
+	if (height_left >= height_right)
+		return (height_left);
+	return (height_right);
+}
+
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to measure the balance factor.
@@ -14,20 +39,16 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance_count = 0, left_node_count = 0,
-		right_node_count = 0;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
-		return (0);
+		return 0;
 
 	if (tree->left != NULL)
-		left_node_count = binary_tree_balance(tree->left) + 1;
+		left_height = binary_tree_height(tree->left) + 1;
 
 	if (tree->right != NULL)
-		right_node_count = binary_tree_balance(tree->right) + 1;
-	if (left_node_count - right_node_count < 1)
-		balance_count = left_node_count - right_node_count;
-	else
-		balance_count = left_node_count - right_node_count;
-	return (balance_count);
+		right_height = binary_tree_height(tree->right) + 1;
+
+	return (left_height - right_height);
 }
