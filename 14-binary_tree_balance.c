73fbfa1f0149952aabed8b281a66d47d68a41138
TobiLight <jgetitdonefast@gmail.com>
2023-08-30 18:42:03 +0100

    Measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -45,10 +45,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		left_height = calculate_tree_height(tree->left);
+		left_height = calculate_tree_height(tree->left) + 1;
 
 	if (tree->right != NULL)
-		right_height = calculate_tree_height(tree->right);
+		right_height = calculate_tree_height(tree->right) + 1;
 
 	return (left_height - right_height);
 }
