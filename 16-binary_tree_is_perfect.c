166469fb7b25ad35f45f007c073c94753de8182f
TobiLight <jgetitdonefast@gmail.com>
2023-08-31 11:16:27 +0100

    Checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,6 +40,7 @@ size_t calculate_tree_height(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left_tree, right_tree;
+
 	if (tree == NULL)
 		return (0);
 
